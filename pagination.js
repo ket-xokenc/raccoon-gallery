7310ca4212af7a24908ef6bb2bea62dd7a5ea494
ket xokenc <prohorovatanya@mail.ru>
2017-11-29 15:37:25 +0200

    refact loop in paginate function

--- pagination.js
+++ pagination.js
@@ -1,11 +1,13 @@
 const NUMBERPERPAGE = 3;
 var pageImgList = new Array();
 var currentPage = 1;
-var galleryImgArray = Array.prototype.slice.call(document.querySelectorAll(".galleryItem"));
-
+var galleryImgArray = Array.prototype.slice.call(
+  document.querySelectorAll(".galleryItem")
+);
+// console.log(typeof(galleryImgArray));
 
 function getNumberOfPages() {
-  return  Math.ceil(IMAGES.length/NUMBERPERPAGE);
+  return Math.ceil(IMAGES.length / NUMBERPERPAGE);
 }
 
 function printPaginationBlock() {
@@ -14,16 +16,16 @@ function printPaginationBlock() {
 
   if (imagesQuantity > NUMBERPERPAGE) {
     // create centered wrapper
-    let centeredBox = document.createElement('div');
+    let centeredBox = document.createElement("div");
     gallery.appendChild(centeredBox);
-    centeredBox.classList.add('center');
+    centeredBox.classList.add("center");
     // create block for links
-    let paginationBox = document.createElement('div');
-    paginationBox.classList.add('pagination');
+    let paginationBox = document.createElement("div");
+    paginationBox.classList.add("pagination");
     centeredBox.appendChild(paginationBox);
     // add links to prev block
     for (let i = 0; i < imagesQuantity; i++) {
-      let pageLink = document.createElement('a');
+      let pageLink = document.createElement("a");
       pageLink.setAttribute("href", "#");
       paginationBox.appendChild(pageLink);
       pageLink.innerHTML = i + 1;
@@ -31,12 +33,12 @@ function printPaginationBlock() {
 
     let prevPageLink = document.createElement("a");
     prevPageLink.setAttribute("href", "#");
-    prevPageLink.innerHTML = '&laquo;';
+    prevPageLink.innerHTML = "&laquo;";
     paginationBox.insertBefore(prevPageLink, paginationBox.firstChild);
 
     let nextPageLink = document.createElement("a");
     nextPageLink.setAttribute("href", "#");
-    nextPageLink.innerHTML = '&raquo;';
+    nextPageLink.innerHTML = "&raquo;";
     paginationBox.appendChild(nextPageLink);
   }
 }
@@ -44,36 +46,40 @@ function printPaginationBlock() {
 printPaginationBlock();
 
 function addActiveClassToPageLink() {
-  let arrayLinks = document.querySelectorAll('.pagination a:not(:first-child):not(:last-child)');
-  for(let i = 0; i < arrayLinks.length; i++) {
-    if(currentPage == arrayLinks[i].textContent) {
-      arrayLinks[i].classList.add('active');
+  let arrayLinks = document.querySelectorAll(
+    ".pagination a:not(:first-child):not(:last-child)"
+  );
+  for (let i = 0; i < arrayLinks.length; i++) {
+    if (currentPage == arrayLinks[i].textContent) {
+      arrayLinks[i].classList.add("active");
     } else {
-      arrayLinks[i].classList.remove('active');
+      arrayLinks[i].classList.remove("active");
     }
   }
 }
 
 function checkDisabledLinks() {
-  let arrayLinks = document.querySelectorAll('.pagination a');
+  let arrayLinks = document.querySelectorAll(".pagination a");
   if (currentPage == 1) {
     // disabled prevbutton
     for (let i = 0; i < arrayLinks.length; i++) {
-      arrayLinks[i].classList.remove('disabled');
+      arrayLinks[i].classList.remove("disabled");
     }
-    document.querySelector('.pagination a:first-child').classList.add('disabled');
-    
+    document
+      .querySelector(".pagination a:first-child")
+      .classList.add("disabled");
   } else if (currentPage == getNumberOfPages()) {
     // disable next button
     for (let i = 0; i < arrayLinks.length; i++) {
-      arrayLinks[i].classList.remove('disabled');
+      arrayLinks[i].classList.remove("disabled");
     }
-    document.querySelector('.pagination a:last-child').classList.add('disabled');
+    document
+      .querySelector(".pagination a:last-child")
+      .classList.add("disabled");
   } else {
     for (let i = 0; i < arrayLinks.length; i++) {
-      arrayLinks[i].classList.remove('disabled');
+      arrayLinks[i].classList.remove("disabled");
     }
-
   }
 }
 
@@ -88,7 +94,7 @@ function prevPage() {
 }
 
 function showPage() {
-  var start = ((currentPage - 1) * NUMBERPERPAGE);
+  var start = (currentPage - 1) * NUMBERPERPAGE;
   var end = start + NUMBERPERPAGE;
   for (i = 0; i < galleryImgArray.length; i++) {
     galleryImgArray[i].classList.add("not-visible");
@@ -106,22 +112,25 @@ function displayImg() {
 }
 
 function pagination(event) {
-  if(event.target != document.querySelector(".pagination a:first-child") && event.target != document.querySelector(".pagination a:last-child") && event.target != document.querySelector('.pagination')) {
-    currentPage = +event.target.textContent;
-    showPage();
-  }
+  let arrayLinks = Array.prototype.slice.call(document.querySelectorAll(".pagination a:not(:first-child):not(:last-child)"));
+  arrayLinks.forEach(function(item, i, arrayLinks) {
+    if(item == event.target) {
+      currentPage = +event.target.textContent;
+      showPage();
+    };
+});
 }
-document.querySelector(".pagination").addEventListener('click', pagination);
+document.querySelector(".pagination").addEventListener("click", pagination);
 
 function load() {
   showPage();
 }
 
-var prevPageLink = document.querySelector('.pagination a:first-child');
-prevPageLink.addEventListener('click', prevPage);
+var prevPageLink = document.querySelector(".pagination a:first-child");
+prevPageLink.addEventListener("click", prevPage);
 
-var nextPageLink = document.querySelector('.pagination a:last-child');
-nextPageLink.addEventListener('click', nextPage);
+var nextPageLink = document.querySelector(".pagination a:last-child");
+nextPageLink.addEventListener("click", nextPage);
 
 var numberOfPages = getNumberOfPages();
 window.onload = load;
